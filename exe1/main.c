6e60fd1ca91ae7da688d1ebf3ff02225dddbac21
kikepuppi <kikepuppi@gmail.com>
2025-03-10 12:44:07 -0300

    ex1 feito

--- exe1/main.c
+++ exe1/main.c
@@ -6,12 +6,18 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int g_timer_0 = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
         flag_f_r = 1;
     } else if (events == 0x8) { // rise edge
     }
+}
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true;
 }
 
 int main() {
@@ -26,10 +32,34 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
+    int timer_0_hz = -1;
+    repeating_timer_t timer_0;
+
+    if (!add_repeating_timer_us(1000000 / timer_0_hz, 
+                                timer_0_callback,
+                                NULL, 
+                                &timer_0)) {
+        printf("Failed to add timer\n");
+    }
+
+    int start = 0;
     while (true) {
 
         if (flag_f_r) {
+
+            if (start){
+                start = 0;
+            } else {
+                start = 1;
+            }
             flag_f_r = 0;
+        }
+        
+        if (start){
+            if(g_timer_0){
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+                g_timer_0 = 0;
+            }
         }
     }
 }
